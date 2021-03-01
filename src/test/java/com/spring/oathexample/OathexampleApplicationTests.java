59c9a8687893a40e2905e74724d34aa9c54185dc
Karson Krieg <klkrieg3@gmail.com>
2021-03-01 13:34:30 -0600

    init commit

---
+++ src/test/java/com/spring/oathexample/OathexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.oathexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OathexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
